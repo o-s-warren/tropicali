b431d90f108c92d7fa34e36c1a711160f9260654
Oliver Warren <66948369+o-s-warren@users.noreply.github.com>
2021-02-08 17:45:56 +0000

    optimised version of landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,58 @@
-function defaultTask(cb) {
+const GulpClient = require("gulp");
+const { watch, series, src, dest } = require("gulp");
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
+
+
+sass.complier = require(`node-sass`);
+
+function runSass() {
   // place code for your default task here
-  cb();
+  // we want to run "css css/app.scss app.css --watch"
+  return src("src/css/app.scss")
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(
+    cleanCSS({
+      compatibility: 'ie8'
+    })
+  )
+  .pipe(sourcemaps.write())
+  .pipe(dest("dist"))
+  .pipe(browserSync.stream());
 }
 
-exports.default = defaultTask
+function html() {
+  return src("src/*.html")
+    .pipe(dest("dist"))
+}
+
+function fonts() {
+  return src("src/fonts/*")
+    .pipe(dest("dist/fonts"))
+}
+
+function images() {
+  return src("src/img/*")
+  .pipe(imagemin())
+  .pipe(dest("dist/img"))
+}
+
+function watchSass() {
+
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+    }
+  })
+
+  watch('src/*.html', html).on("change", browserSync.reload)
+  watch('src/css/app.scss', runSass)
+  watch('src/fonts/*', fonts)
+  watch('src/img/*', images)
+}
+
+exports.default = series(html, runSass, fonts, images, watchSass);
